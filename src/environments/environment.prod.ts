9c85fdafe2a2a306b1e756cb61cd59a8007d3127
sreeparnadas <sreeparnadas0675@gmail.com>
2022-01-29 08:50:48 +0530

    prod url updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  BASE_API_URL: '/alpha_project/alpha_api/public/api',
-  BASE_PUBLIC_URL: '/alpha_project/alpha_api/public',
+  BASE_API_URL: 'https://new.sikkimteer.com/alpha_api/public/api',
+  // BASE_API_URL: '/alpha_project/alpha_api/public/api',
+  // BASE_PUBLIC_URL: '/alpha_project/alpha_api/public',
 };
